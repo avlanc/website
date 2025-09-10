4dce5f79095eb974ee737bf68574c1c2b375914e
Samarth Sharma <110784602+bugsum@users.noreply.github.com>
2025-09-11 02:31:59 +0530

    feat(waitlist): add a waitlist with email signup (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,93 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { toast } from 'sonner';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!email) return;
+
+    try {
+      setLoading(true);
+
+      const res = await fetch('/api/waitlist', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+      });
+
+      if (!res.ok) {
+        throw new Error('Failed to join waitlist');
+      }
+
+      setEmail('');
+      toast.success('Successfully joined the waitlist!');
+      console.log('Success: ', await res.json());
+    } catch (error) {
+      toast.error('Failed to join the waitlist. Please try again.');
+      console.error('Error joining waitlist: ', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <section className="relative flex h-full min-h-screen w-screen flex-col items-center justify-center overflow-hidden py-32">
+      <div className="absolute inset-0 -z-10 h-full w-full">
+        <Image
+          src="/background.jpeg"
+          fill
+          priority
+          className="object-cover object-center select-none"
+          alt="Background Image"
+        />
+        <div className="absolute inset-0 bg-black/40" />
+      </div>
+
+      <h2 className="relative py-2 text-center font-sans text-5xl font-extrabold tracking-tight md:py-10 lg:text-8xl">
+        Join the Waitlist
+      </h2>
+
+      <p className="text-md mx-auto px-2 text-center md:max-w-xl lg:text-lg">
+        Our new website is currently under development. Join the waitlist to be
+        the first to know when we launch! We appreciate your patience and
+        support.
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="relative mt-4 flex w-full max-w-md flex-col items-center gap-3 rounded-full p-4 lg:flex-row lg:p-1"
+      >
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="dark:bg-input/70 h-12 w-full rounded-md border-1 ring-0 focus-visible:ring-0 focus-visible:outline-none active:ring-0 active:outline-0"
+          placeholder="Enter your email"
+          type="email"
+          required
+        />
+
+        <Button
+          type="submit"
+          disabled={!email || loading}
+          className={cn(
+            'h-12 w-full rounded-md lg:w-fit',
+            'ring-0 focus-visible:ring-0 focus-visible:outline-none active:ring-0 active:outline-0'
+          )}
+          size="lg"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          {loading ? 'Joining...' : 'Join the Waitlist'}
+        </Button>
+      </form>
+    </section>
   );
 }
